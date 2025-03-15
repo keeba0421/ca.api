a235a3ef56a47ccff7dceb8e0e246dc36241dc00
keeba0421 <omgkeeba0421@gmail.com>
2025-03-16 02:19:27 +0900

    Update script1.js

--- script1.js
+++ script1.js
@@ -1,6 +1,6 @@
 const user_name = encodeURIComponent("Example".toLowerCase());
-const world_name = "scania"; // API에서 지원하는 값인지 확인
-const apiKey = "your-api-key-here"; // 유효한 키로 교체
+const world_name = "해피"; // API에서 지원하는 값인지 확인
+const apiKey = "test_c23aa4b3679c9dc52214856752d895edb607ed3d30af633807b946b8e49680ecefe8d04e6d233bd35cf2fabdeb93fb0d"; // 유효한 키로 교체
 const url = `https://api.nexon.com/users?user_name=${user_name}&world_name=${world_name}`;
 const headers = {
   "x-nxopen-api-key": apiKey
